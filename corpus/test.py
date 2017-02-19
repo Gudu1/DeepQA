339daaf581293fc0f1c84a420d0447d2af488ba4
mug2mag <magic2116@126.com>
2017-02-19 12:31:06 +0800

    #1 gu,you can use the extract_data.py to insert data directly

--- corpus/test.py
+++ corpus/test.py
@@ -21,3 +21,15 @@ else:
 "rou/jirou/","rou/yarou/","rou/yurou/","rou/hongshaorou/","rou/paigu/","rou/jidan/","rou/yadan/","mian/miantiao/","mian/mantou/","mian/baozi/", "mian/mianshi/","mian/fen/",\
 "mian/mixian/","mian/jiaozizuofa/","mian/gjf/","mian/zhou/","caipu/zaocan/","caipu/wucan/","caipu/wancan/","caipu/xiafan/","caipu/kuaishou/","caipu/jiachangcai/","caipu/recai/","caipu/huncai/","caipu/liangcai/","caipu/xiaochi/",\
 "caixi/lucai/","caixi/chuancai/","caixi/yuecai/","caixi/mincai/","caixi/sucai/","caixi/zhecai/","caixi/xiangcai/","caixi/huicai/","caixi/jingcai/shc/","rou/zhurou/",
+
+
+
+
+ ["xiaochi/bfxc/","xiaochi/nfxc/",\
+"xiaochi/scxc/","xiaochi/chongq/","xiaochi/bjxc/","xiaochi/tjxc/","xiaochi/twxc/","xiaochi/hk/",\
+"xiaochi/shanx/","xiaochi/hanxi/","xican/yidali/","xican/faguocai/","xican/taiguocai/","xican/yindu/","xican/hanguocai/","xican/haixian/","xican/xibanya/","xican/dny/","xican/riben/",\
+"xican/shousi/","tang/jidantang/","tang/gedatang/","tang/jiyutang/","tang/haixiantang/","tang/paigutang/","tang/shoushentang/","tang/meirong/","tang/yangsheng/","tang/tianpin/","tang/dunpin/",\
+"hongbei/mianbao/","hongbei/binggan/","hongbei/quqi/","hongbei/dangao/","hongbei/tusi/","hongbei/pizza/","hongbei/yuebing/","jiankang/youer/","jiankang/yinger/","jiankang/er/","jiankang/baoshipu/",\
+"jiankang/beiyun/","jiankang/chan/","jiankang/muying/","jiankang/laoren/","pengren/shaokao/","pengren/huoguo/","pengren/zhengzhi/","pengren/dunzhi/","pengren/yanzhi/","pengren/zhazhi/","pengren/luzhi/",\
+"pengren/xunzhi/","pengren/jian/","pengren/hongshao/","kouwei/mala/","kouwei/suanla/","kouwei/wuxiang/","kouwei/qingdan/","kouwei/tiangcu/","kouwei/jiangx/","kouwei/suanxiang/","kouwei/kali/",\
+"kouwei/ziran/","kouwei/jiaoyan/","jieri/nianye/","jieri/zhongqiu/","jieri/tangyuanzuofa/","jieri/laba/","jieri/duanwujie/","jieri/chongy/","jieri/chun/","jieri/xia/","jieri/qiu/","jieri/dong/"]
