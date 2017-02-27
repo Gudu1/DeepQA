84c3a47a63a01815487ba7d59c683c7d5e3e0453
mug2mag <magic2116@126.com>
2017-02-27 16:21:06 +0800

    #1 complete the corpus, conversations.txt and lines.txt

--- get_corpus/extract_data.py
+++ get_corpus/extract_data.py
@@ -1,13 +1,13 @@
 import pymysql
 
-conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='123456', db='shortdata2.1', charset='utf8')
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='123456', db='shortdata2', charset='utf8')
 cursor = conn.cursor()
 
-sql = "select * from `shortdata2.1`.short_corpus"
+sql = "select * from `shortdata2`.short_corpus"
 cursor.execute(sql)
 data = cursor.fetchall()
 
-with open("shortdata2.1_shortcorpus.txt","w",encoding="utf8") as fp:
+with open("shortdata2_shortcorpus.txt","w",encoding="utf8") as fp:
     for i in data:
         fp.writelines(str(list(i))+"\n")
 
---
+++ get_corpus/make_data2.1_conversation.py
@@ -0,0 +1,28 @@
+conversations_text = open("conversations.txt", 'a')
+lineids_text = open("lineids.txt", 'a')
+database_num = ["2","2.1"]
+lineid = 0 
+for i in database_num:
+	with open("shortdata{}_shortcorpus.txt".format(i),'r',encoding="utf8") as fp:
+		items = fp.readlines()
+		for i in items:
+			spliters = [x.lstrip("'[").rstrip("']\n") for x in i.split(", ")]
+			question = spliters[1]
+			print("question"+question)
+			answers = spliters[2]
+			split_answer = [j.lstrip(r"\\r\\t\\u3000\\u3000").rstrip("r\\").replace("\\xa0\\xa0",'').replace("xa0",'') for  j in answers.split(r"\n")[1:]]
+			while "" in split_answer:
+				split_answer.remove("")
+			split_answer_again1 = [n.split("。")[0] for n in split_answer]
+			split_answer_again2= [n.split("？")[0].strip() for n in split_answer_again1]
+			lineids = []
+			for m in split_answer_again2:
+					lines_write_in = str(lineid) + " +++$+++ " + m + "\n"
+					lineids_text.write(lines_write_in)
+					lineids.append(lineid)
+					lineid = lineid + 1
+			conversations_text.write("con"+" +++$+++ "+str(lineids)+"\n")
+    			
+ 
+conversations_text.close()
+lineids_text.close()
